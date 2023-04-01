bedd9529aa48e64041710c2b8f191652c54c9485
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2023-04-01 23:13:37 +0530

    Update Component.js

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
+import "../styles/App.css";
+import Component from "./Component";
+import ErrorBoundary from "./ErrorBoundary";
+const App = () => {
+  let setFunc;
 
-const Component = (props) => {
-  const [error, setError] = useState(false);
+  let changeFunc = (func) => {
+    setFunc = func;
+  };
 
-  props.change(setError);
+  const errorGenerator = () => {
+    setFunc(true);
+  };
 
-  if (error) {
-    throw new Error("Error");
-  }
-
-  return <></>;
+  return (
+    <div id="main">
+      <h2>React Error Boundaries</h2>
+      <ErrorBoundary>
+        <Component change={changeFunc} />
+      </ErrorBoundary>
+      <button id="gen" onClick={errorGenerator}>
+        Generate Error
+      </button>
+    </div>
+  );
 };
 
-export default Component;
+export default App;
